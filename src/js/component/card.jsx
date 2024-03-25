6f7c180e8467f882ab439c151a0fcc8a10957758
Carla <152617155+Cl1493@users.noreply.github.com>
2024-03-25 20:00:44 +0000

    Bootstrap de Card

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 
-
 const Card = (props) => {
   const {store, actions} =useContext(Context);
   const handleDelete = () => {
@@ -11,18 +10,31 @@ const Card = (props) => {
   }    
 
     return (
-    <div className="card">
-      <div className="card-body">
-        <p>{props.nombre}</p>
-        <p>{props.email}</p>
-        <p>{props.phoneNumber}</p>
-        <p>{props.address}</p>
-        <Link to={`/editForm/${props.id}`}>
-          <button className="position-relative m-3 py-2 px-4 btn btn-success" >Editar</button>
-        </Link>
-        <button className="position-relative m-3 py-2 px-4 btn btn-success"  onClick={()=>{handleDelete()}}>Eliminar</button>
+    <div className="card m-3">
+      <div className="row p-3">
+        <div className="col-4">
+          <img src="https://www.chaomanagement.com/wp-content/uploads/2019/08/Pepe-Viyuela-053-e1607963190390ok-770x539.jpg" className="img-fluid rounded-circle m-auto" alt="..."/>
+      </div>
+      <div className="col-6 m-auto">  
+        <div className="card-body">
+          <h5 className="card-title text-start fs-2">{props.nombre}</h5>
+          <p className="card-text text-body-tertiary text-start fs-5">✉ {props.email}</p>
+          <p className="card-text text-body-tertiary text-start fs-5">✆ {props.phoneNumber}</p>
+          <p className="card-text text-body-tertiary text-start fs-5">📫 {props.address}</p>
+        </div>
+      </div>  
+      <div className="col-2 m-auto">
+          <Link to={`/editForm/${props.id}`}>
+            <button className="position-relative m-3 py-2 px-4 btn btn-success" >Editar</button>
+          </Link>
+          <button className="position-relative m-3 py-2 px-3 btn btn-success"  onClick={()=>{handleDelete()}}>Eliminar</button>
+      </div>
+      
       </div>
     </div>
     )}
 
     export default Card;
+
+
+   
